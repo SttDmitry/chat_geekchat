66110b91e3ce8829f85aa90468113583917e9160
SttDmitry <sheicko.dmitry@yandex.ru>
2021-01-31 11:37:21 +0300

    hw2

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -17,7 +17,7 @@ public class Server {
 
     public Server() {
         clients = new CopyOnWriteArrayList<>();
-        authService = new SimpleAuthService();
+        authService = new DataBaseAuthService();
 
         try {
             server = new ServerSocket(PORT);
